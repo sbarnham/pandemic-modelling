3bf2e225cee9f0e8e2445685abafe78d32ee550a
sbarnham <barnham.s@etoncollege.org.uk>
2020-05-25 13:11:22 +0100

    added two new lines of data (susceptible and removed). made slight formatting alterations to chart.

--- PandemicModelling/ViewController.swift
+++ PandemicModelling/ViewController.swift
@@ -11,21 +11,23 @@ import Charts
 
 class ViewController: UIViewController {
     
+    
     @IBOutlet var lineChart: LineChartView!
 
-    
-    var aspects = Aspects()
-    var modeller = Modeller()
-    var dataEntries: [ChartDataEntry] = [ChartDataEntry(x: 0, y: 0)]
-    var day: Double = 0
-    
-    
     @IBOutlet var susceptibleLabel: UILabel!
     
     @IBOutlet var infectedLabel: UILabel!
     
     @IBOutlet var removedLabel: UILabel!
     
+    var aspects = Aspects()
+    var modeller = Modeller()
+    var infectedDataEntries: [ChartDataEntry] = []
+    var susceptibleDataEntries: [ChartDataEntry] = []
+    var removedDataEntries: [ChartDataEntry] = []
+    var day: Double = 0
+    
+    //Setup of chart modifications and formats
     override func viewDidLoad() {
         super.viewDidLoad()
         susceptibleLabel.text = "Susceptible: nil"
@@ -34,12 +36,16 @@ class ViewController: UIViewController {
         lineChart.backgroundColor = .white
         lineChart.drawGridBackgroundEnabled = false
         lineChart.rightAxis.enabled = false
-        lineChart.leftAxis.labelFont = .boldSystemFont(ofSize: 12)
         lineChart.xAxis.labelPosition = .bottom
         lineChart.xAxis.labelFont = .boldSystemFont(ofSize: 12)
         lineChart.xAxis.axisMinimum = 0
-        lineChart.xAxis.spaceMax = 0.1
+        lineChart.xAxis.spaceMax = 1
+        lineChart.xAxis.granularity = 1
+        lineChart.xAxis.drawGridLinesEnabled = false
         lineChart.leftAxis.axisMinimum = 0
+        lineChart.leftAxis.labelFont = .boldSystemFont(ofSize: 12)
+        lineChart.leftAxis.granularity = 1
+        lineChart.leftAxis.drawGridLinesEnabled = false
         updateChartData()
         
         // Do any additional setup after loading the view.
@@ -48,6 +54,9 @@ class ViewController: UIViewController {
     func simulation() {
         modeller.r = aspects.r0
         modeller.susceptible = (aspects.population - 1)
+        susceptibleDataEntries.append(ChartDataEntry(x: 0, y: Double(modeller.susceptible)))
+        infectedDataEntries.append(ChartDataEntry(x: 0, y: 1))
+        removedDataEntries.append(ChartDataEntry(x: 0, y: 0))
         Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
             if self.modeller.infected == 0 {
                 timer.invalidate()
@@ -57,28 +66,41 @@ class ViewController: UIViewController {
             self.infectedLabel.text = "Infected: \(tuple.1)"
             self.removedLabel.text = "Removed: \(tuple.2)"
             self.day += 1
-            self.dataEntries.append(ChartDataEntry(x: self.day, y: Double(tuple.1)))
+            self.susceptibleDataEntries.append(ChartDataEntry(x: self.day, y: Double(tuple.0)))
+            self.infectedDataEntries.append(ChartDataEntry(x: self.day, y: Double(tuple.1)))
+            self.removedDataEntries.append(ChartDataEntry(x: self.day, y: Double(tuple.2)))
             self.updateChartData()
         }
         
     }
     
     @IBAction func simulateButton(_ sender: Any) {
+        lineChart.clearValues()
         simulation()
     }
     
-    func updateChartData() {
-        let chartDataSet = LineChartDataSet(entries: dataEntries, label: "Infected")
-        let chartData = LineChartData(dataSet: chartDataSet)
-        chartDataSet.mode = .cubicBezier
+    
+    //Formatting each line of data. Required after every alteration of data (due to how the library works).
+    fileprivate func dataSetFormatting(chartDataSet: LineChartDataSet, colour: UIColor) {
+        chartDataSet.mode = .horizontalBezier
         chartDataSet.lineWidth = 3
         chartDataSet.circleRadius = 2.5
-        chartDataSet.setColor(.red)
-        chartDataSet.fill = Fill(color: .red)
+        chartDataSet.setColor(colour)
+        chartDataSet.fill = Fill(color: colour)
         chartDataSet.drawFilledEnabled = true
-        chartDataSet.setCircleColor(.red)
+        chartDataSet.setCircleColor(colour)
+    }
+    
+    //Updates chart data after each call.
+    func updateChartData() {
+        let infChartDataSet = LineChartDataSet(entries: infectedDataEntries, label: "Infected")
+        let susChartDataSet = LineChartDataSet(entries: susceptibleDataEntries, label: "Susceptible")
+        let remChartDataSet = LineChartDataSet(entries: removedDataEntries, label: "Removed")
+        let chartData = LineChartData(dataSets: [susChartDataSet, infChartDataSet, remChartDataSet])
+        dataSetFormatting(chartDataSet: infChartDataSet, colour: .red)
+        dataSetFormatting(chartDataSet: susChartDataSet, colour: .blue)
+        dataSetFormatting(chartDataSet: remChartDataSet, colour: .gray)
         lineChart.data = chartData
-        
         
         
     }
